261b4c3bfcfa99bc72b9f8e88b860c3a3160d5de
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-05-21 20:54:56 -0300

    #08 Curso PHP Micro Framework do zero - Fluxograma do carregamento inicial

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,7 @@
 <?php
 
 $routes[] = ['/', 'HomeController@index'];
+$routes[] = ['/posts', 'PostsController@index'];
+$routes[] = ['/posts/{id}/show', 'PostsController@show'];
+
 return $routes;
--- core/Route.php
+++ core/Route.php
@@ -10,12 +10,29 @@ class Route
     
    public function __construct(array $routes)
    {
-     $this->routes = $routes;
+      $this->routes = $routes;
+      $this->run();
    }
     
-   public function getUrl()
+   private function getUrl()
    {
-       return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+      return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
 
    }
+   
+   private function run()
+   {
+      $url = $this->getUrl();
+      $urlArray = explode('/', $url);
+      print_r( $urlArray );
+      
+      foreach($this->routes as $route){
+         $routeArray = explode('/', $route[0]);
+         echo "<br/>";
+         print_r ($routeArray);
+      }
+   }
+   
+   
+   
 }
