fce93265592f79eaea3cbe09c981fd73b2258f31
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-05-21 22:30:34 -0300

    #13 Curso PHP Micro Framework do zero - Criando primeira View

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,8 +4,16 @@ namespace App\Controllers;
 
 class HomeController
 {
+   private $view;
+   
+   public function __construct()
+   {
+      $this->view = new \stdClass;
+      $this->view->nome = "Wanderlei Hüttel";
+   }
+   
    public function index()
    {
-      echo "Home";
+      require_once __DIR__ . "/../Views/home/index.phtml";
    }   
 }
