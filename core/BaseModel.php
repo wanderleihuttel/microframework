482ca095d05226302a0cef8dfed0cc1ab1c7f568
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-05-25 21:59:09 -0300

    #19 Curso PHP Micro Framework do zero - Criando a camada Model

---
+++ core/BaseModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+abstract class BaseModel
+{
+	private $pdo;
+	protected $table;
+
+	public function __construct(PDO $pdo)
+	{
+		$this->pdo = $pdo;
+	}
+
+	public function All()
+	{
+		$query = "SELECT * FROM {$this->table}";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		$stmt->closeCursor();
+		return $result;
+	}
+}
