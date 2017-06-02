0de06d235c3a41f338109ab7ac4271d164b42463
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-06-01 22:07:38 -0300

    #38 Curso PHP - Micro Framework do zero - Validações

---
+++ core/Validator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+
+class Validator
+{
+   public static function make(array $data,array $rules)
+   {
+      foreach ($rules as $ruleKey as $ruleValue){
+         foreach ($data as $dataKey as $dataValue){
+            if($ruleKey == $dataKey){
+               switch ($ruleValue){
+                  case 'required':
+                     if($dataValue=='' || empty($dataValue)){
+                        $errors[$ruleKey] = "O campo {$ruleKey} deve preenchido."
+                     }
+                     break;
+                  case 'email':
+                     if(!filter_var($dataValue,FILTER_VALIDATE_EMAIL)){
+                        $errors[$ruleKey] = "O campo {$ruleKey} não é válido.";
+                     }
+                     break;
+                  case 'float':
+                     if(!filter_var($dataValue,FILTER_VALIDATE_FLOAT)){
+                        $errors[$ruleKey] = "O campo {$ruleKey} deve conter um número decimal.";
+                     }
+                     break;
+                  case 'integer':
+                     if(!filter_var($dataValue,FILTER_VALIDATE_INT)){
+                        $errors[$ruleKey] = "O campo {$ruleKey} deve conter um número inteiro.";
+                     }
+                     break;
+               }
+            }
+         }
+      }
+   }
+   
+}
